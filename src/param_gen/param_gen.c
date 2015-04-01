e8f0a3d3295e01e09fc41e787bdd9d9a38d7af81
20logTom <haspl.tom@gmail.com>
2015-04-01 15:45:52 +0200

    finished parameter generator and deleted template

--- src/param_gen/param_gen.c
+++ src/param_gen/param_gen.c
@@ -14,8 +14,9 @@ extern int generate_params_file (unsigned int const block_size,
 	FILE* fd;
 	char outfile[40];
 	char prefix[30];
+	char line[50];
 
-	sprintf (prefix, "params%d-%d-%d-%d-%d-%d", block_size, block_step, sigma, max_blocks, h_search, v_search);
+	sprintf (prefix, "params_%d-%d-%d-%d-%d-%d", block_size, block_step, sigma, max_blocks, h_search, v_search);
 
 	// set filename for parameter file
 	if (get_output_filename (outfile, "res/", prefix, "txt", 0) != 0) {
@@ -23,7 +24,54 @@ extern int generate_params_file (unsigned int const block_size,
 		return 1;
 	}
 
-	printf ("%s\n", outfile);
+	// open file for append
+	fd = fopen (outfile, "w");
+	if (fd == NULL) {
+		generate_error ("Unable to open parameter-file for writing...");
+		return 1;
+	}
+
+	sprintf (line, "# file:        %s", outfile);
+	fprintf (fd, "%s\n", line);
+	
+	sprintf (line, "# author:      Thomas Haspl");
+	fprintf (fd, "%s\n", line);
+
+	sprintf (line, "# date:        2015-04-01");
+	fprintf (fd, "%s\n", line);
+
+	sprintf (line, "# description: Config-File for the BM3D denoising algorithm");
+	fprintf (fd, "%s\n\n", line);
+
+	sprintf (line, "BEGIN_PARAMS");
+	fprintf (fd, "%s\n", line);
+
+	sprintf (line, "	BLOCK_SIZE %d", block_size);
+	fprintf (fd, "%s\n", line);
+
+	sprintf (line, "	BLOCK_STEP %d", block_step);
+	fprintf (fd, "%s\n", line);
+
+	sprintf (line, "	SIGMA %d", sigma);
+	fprintf (fd, "%s\n", line);
+
+	sprintf (line, "	MAX_BLOCKS %d", max_blocks);
+	fprintf (fd, "%s\n", line);
+
+	sprintf (line, "	H_SEARCH %d", h_search);
+	fprintf (fd, "%s\n", line);
+
+	sprintf (line, "	V_SEARCH %d", v_search);
+	fprintf (fd, "%s\n", line);
+
+	sprintf (line, "END_PARAMS");
+	fprintf (fd, "%s", line);
+
+	// close file again
+	if (fclose(fd) == EOF) {
+		generate_error ("Unable to close parameter-file...");
+		return 1;
+	}
 
 	return 0;
 }
