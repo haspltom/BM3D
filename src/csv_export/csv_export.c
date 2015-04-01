af21a563ea8f40ab369b57e18c87456694d6c914
20logTom <haspl.tom@gmail.com>
2015-04-01 09:22:55 +0200

    added src/csv_export/ with header and source file

---
+++ src/csv_export/csv_export.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <regex.h>
+#include "../error/error.h"
+#include "csv_export.h"
+
---
+++ src/csv_export/csv_export.h
@@ -0,0 +1,5 @@
+#ifndef CSV_EXPORT_H
+#define CSV_EXPORT_H
+
+
+#endif // CSV_EXPORT_H
