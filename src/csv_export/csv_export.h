c4ae4625b5b8e0187a0237c1977ee4fba8b91578
20logTom <haspl.tom@gmail.com>
2015-05-26 12:42:09 +0200

    deleted module csv_export

--- src/csv_export/csv_export.h
+++
@@ -1,6 +0,0 @@
-#ifndef CSV_EXPORT_H
-#define CSV_EXPORT_H
-
-extern int add_csv_line (char* const filename, unsigned int const groups, unsigned int const block_size, double const time);
-
-#endif // CSV_EXPORT_H
